b1a2cab5a0593b0c540097594fb06376f24d854f
Alexei-Cheronko <dr.devilby@gmail.com>
2022-08-28 21:13:57 +0300

    init(hw_1): finish task1

---
+++ task1/scriptMy.js
@@ -0,0 +1,57 @@
+// С ниже приведенным объектом решить следующие задачи:
+
+// 1. Создать строку из названий предметов написаных через запятую
+// 2. Посчитать общее количнство студентов и учителей на всех предметах
+// 3. Получить среднее количество студентов на всех пердметах
+// 4. Создать массив из объектов предметов
+// 5. Получить массив из предметов и отсортировать по количеству преподавателей на факультете от большего к меньшему
+
+const subjects = {
+    mathematics: {
+        students: 200,
+        teachers: 6
+    },
+    biology: {
+        students: 120,
+        teachers: 6
+    },
+    geography: {
+        students: 60,
+        teachers: 2
+    },
+    chemistry: {
+        students: 100,
+        teachers: 3
+    }
+}
+
+// 1.
+const neWsubjects = Object.keys(subjects);
+console.log(neWsubjects.join(', '));
+
+// 2.
+const sumStudents = Object.values(subjects).reduce(
+(acc, subjects) => acc + subjects.students,
+0
+);
+console.log(sumStudents);
+
+const sumTeachers = Object.values(subjects).reduce(
+(acc, subjects) => acc + subjects.teachers,
+0
+);
+console.log(sumTeachers);
+
+// 3.
+let middleStudents = sumStudents / Object.values(subjects).length;
+console.log(middleStudents);
+
+// 4.
+const newArray = Object.values(subjects);
+console.log(newArray);
+
+// 5.
+const sorting = newArray.sort((a, b) => {
+return b.teachers - a.teachers;
+});
+console.log(sorting);
