2043bb3a561e87a2b023c616c8b68de7a6822b8e
Aleksandrs Gluhovs <aleksandrs.gluhovs@yandex.ru>
2022-09-06 18:31:12 +0100

    fix(hw_1):

--- task1/hw/index.js
+++ task1/hw/index.js
@@ -22,13 +22,11 @@ const subjectsKeys = Object.keys(subjects);
 console.log(subjectsKeys.join(', '));
 
 // Task 2
-const sumStudents = Object.values(subjects)
-  .reduce((acc, subjects) => acc + subjects.students, 0);
+const sumStudents = Object.values(subjects).reduce((acc, subjects) => acc + subjects.students, 0);
 console.log(sumStudents);
-
-const sumTeachers = Object.values(subjects)
-  .reduce((acc, subjects) => acc + subjects.teachers, 0);
+const sumTeachers = Object.values(subjects).reduce((acc, subjects) => acc + subjects.teachers, 0);
 console.log(sumTeachers);
+
 // Task 3
 const avrgStudents = sumStudents / Object.values(subjects).length;
 console.log(avrgStudents);
